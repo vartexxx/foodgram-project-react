c089c56386a119eeba1f5078bec04d0343921861
vartexxx <vartexxx29@yandex.ru>
2023-04-14 14:03:48 +0500

    fix: исправлен скрипт для json

--- backend/script.py
+++ backend/script.py
@@ -27,22 +27,22 @@ try:
         port=DB_PORT,
     )
     cursor = connection.cursor()
-    json_file = open(
+    data = json.load(open(
         './data/ingredients.json',
         'r',
         encoding='utf8',
-    )
-    data = json.load(json_file)
-    for line in data:
-        title = line.get('name')
-        measurement_unit = line.get('measurement_unit')
+    ))
+    for str in data:
         cursor.execute(
-            f"INSERT INTO recipes_ingredientsmodel("
-            f"name, measurement_unit"
-            f") VALUES ('{title}', '{measurement_unit}');")
+            f"INSERT INTO recipes_ingredient("
+            f"name, measurement_unit)"
+            f"VALUES ('{str.get('name')}', '{str.get('measurement_unit')}');")
     connection.commit()
-    cursor.close()
-    connection.close()
     print('Данные успешно внесены в базу данны PostgreSQL')
-except (Exception, Error) as error:
-    print("Ошибка при работе с PostgreSQL", error)
+except (Exception, Error) as err:
+    print("Ошибка при работе с PostgreSQL", err)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
