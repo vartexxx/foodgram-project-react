3421f871fda3566545eefc980891d681d7144c02
vartexxx <vartexxx29@yandex.ru>
2023-04-26 07:38:26 +0500

    fix: favorited to favorites

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -28,7 +28,7 @@ class RecipesFilter(FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(favorite__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
